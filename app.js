cebbee5dafc530e4f847a20f9e4d870ce339c290
avantikainfotech01-pixel <avantikainfotech01@gmail.com>
2025-10-08 18:59:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.use("/api/kyc", kycRoutes);
 app.use("/api/point", pointRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
