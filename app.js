538e202795b61b526805affc57dd1e6d426e37f0
avantikainfotech01-pixel <avantikainfotech01@gmail.com>
2025-10-09 16:27:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,4 @@ app.use("/api/wallet", walletRoutes);
 app.use("/api/kyc", kycRoutes);
 app.use("/api/point", pointRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000, "0.0.0.0", () => console.log("Server running on port 3000"));
