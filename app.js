ea6162d1ab3f17b2f7254d6c0544776ad75f9f54
avantikainfotech01-pixel <avantikainfotech01@gmail.com>
2025-10-13 11:53:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose
-  .connect(process.env.MONGO_URI, {})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Error:", err));
+  .catch(err => console.error("MongoDB Error:", err));
 
 app.get("/", (req, res) => {
   res.send("Welcome to the QR Code API");
