e461161e0080481438a80430a1399914018e8305
avantikainfotech01-pixel <avantikainfotech01@gmail.com>
2025-11-17 18:09:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"],
     credentials: true,
   })
 );
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
