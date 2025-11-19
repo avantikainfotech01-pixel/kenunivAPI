e961487b844601f3b41e0ba34db756113ac288c0
avantikainfotech01-pixel <avantikainfotech01@gmail.com>
2025-11-19 16:52:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
   origin: "*",
   methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"],
+  allowedHeaders: "*",
   credentials: true
 }));
 
