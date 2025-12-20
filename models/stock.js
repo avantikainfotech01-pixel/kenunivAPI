8749fd1c1a5fb92c48dd44689f642f80b441d4dc
avantikainfotech01-pixel <avantikainfotech01@gmail.com>
2025-12-20 12:14:33 +0530

    Update stock.js

--- models/stock.js
+++ models/stock.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
-const stockSchema = new mongoose.Schema({
-  itemName: { type: String, required: true },
-  quantity: { type: Number, default: 0 },
-  minQty: { type: Number, default: 0 },
-  schemeId: { type: mongoose.Schema.Types.ObjectId, ref: "Scheme" },
-  createdAt: { type: Date, default: Date.now },
-});
+const stockSchema = new mongoose.Schema(
+  {
+    itemName: { type: String, required: true },
+    quantity: { type: Number, default: 0 },
+    minQty: { type: Number, default: 0 },
+    schemeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Scheme",
+      required: true,
+      unique: true, // 🔴 VERY IMPORTANT
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Stock", stockSchema);
